7226c7f973d86923f2312e4443ad0e9f22b8b3b9
SwapnilPramanik109 <74817985+SwapnilPramanik109@users.noreply.github.com>
2021-02-20 09:42:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+color = "red";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.rect(150, 143, 430, 200,);
+ctx.stroke();
+
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+
